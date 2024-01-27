fb8e371b384685f4cce2990172ae019787846979
albashala <albashala2000@gmail.com>
2024-01-27 11:16:54 +0100

    Error Handling

--- BL/app.js
+++ BL/app.js
@@ -4,23 +4,36 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-var cors = require('cors');
+const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
 
 
-
-//database connection
+// Database connection
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
- 
 })
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err));
 
+// Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
 
-//port
-const port = process.env.PORT || 9000
+//error middleware
+app.use(errorHandler);
 
+// Port
+const port = process.env.PORT || 9000;
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
