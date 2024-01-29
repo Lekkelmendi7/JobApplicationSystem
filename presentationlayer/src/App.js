d4c79a331ac94152aa352020c2cf923503740069
albashala <albashala2000@gmail.com>
2024-01-29 14:23:06 +0100

    Basic React Component
    Navbar, Header, Footer using Material UI

---
+++ presentationlayer/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import NotFound from './pages/NotFound';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+
+const App = () => {
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+      <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='*' element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
+    </>
+  )
+}
+
+export default App
